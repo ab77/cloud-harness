98f1f8b7f157abf3104bd5abf4ce53a66e15d4ab
Anton Belodedenko <anton.belodedenko@gmail.com>
2015-11-14 22:53:08 +0000

    Create netflix-proxy.sh

---
+++ netflix-proxy.sh
@@ -0,0 +1,4 @@
+#!env bash
+
+# configure netflix-proxy and restart Docker containers
+git clone https://github.com/ab77/netflix-proxy /opt/netflix-proxy && /opt/netflix-proxy/build.sh -c 127.0.0.1 -i 1 -d 1 && docker restart bind sniproxy
