bbd4a0e127002205bca7a0af89cd70251447b2fe
Anton Belodedenko <anton.belodedenko@gmail.com>
2016-03-25 14:47:29 +0000

    Delete netflix-proxy.sh

--- netflix-proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# build netflix-proxy
-rm -rf /opt/netflix-proxy && \
-  git clone https://github.com/ab77/netflix-proxy /opt/netflix-proxy && \
-  /opt/netflix-proxy/build.sh -c 127.0.0.1 -i 1 -d 1 -t 1
